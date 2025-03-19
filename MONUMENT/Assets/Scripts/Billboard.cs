fd95911537e1190aec2f3c5c11f6239fa277e992
Dan-Mechanics <github.tim@desmidt.org>
2025-03-19 18:14:09 +0100

    billboard

---
+++ MONUMENT/Assets/Scripts/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using VogueVR.Heartbeat;
+
+namespace VogueVR.Composite
+{
+    public class Billboard : SelfSubscriber, ITickable
+    {
+        [SerializeField] private Transform cam = default;
+
+        public void DoTick()
+        {
+            this.transform.LookAt(this.cam.position, Vector3.up);
+        }
+    }
+}
