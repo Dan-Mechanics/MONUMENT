7ac9a3e922731f50709f2446ad5ccba5ccfc3e29
Dan-Mechanics <github.tim@desmidt.org>
2025-03-23 13:13:27 +0100

    dwdwd

---
+++ MONUMENT/Assets/FadeOut.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace MONUMENT
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class FadeOut : MonoBehaviour
+    {
+        [SerializeField] private bool onStart = default;
+
+        [SerializeField] private UnityEvent onDone = default;
+        [SerializeField] private float speed = 1f;
+
+        private CanvasGroup canvasGroup;
+        private bool isFading;
+
+        private void Awake()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        private void Start()
+        {
+            if (onStart)
+                Fade();
+        }
+
+        private void Update()
+        {
+            if (!isFading)
+                return;
+            
+            canvasGroup.alpha -= Time.deltaTime * speed;
+
+            if (canvasGroup.alpha <= 0f) 
+            {
+                onDone?.Invoke();
+                isFading = false;
+                canvasGroup.alpha = 0f;
+                canvasGroup.blocksRaycasts = false;
+            }
+        }
+
+        public void Fade() 
+        {
+            if (isFading)
+                return;
+
+            canvasGroup.blocksRaycasts = true;
+            isFading = true;
+            canvasGroup.alpha = 1f;
+        }
+    }
+}
