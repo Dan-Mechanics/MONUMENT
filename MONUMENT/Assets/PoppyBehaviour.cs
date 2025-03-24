a26defdf2e47560e5eabb3dd100bd637e833f167
Dan-Mechanics <github.tim@desmidt.org>
2025-03-24 13:14:46 +0100

    poppy spawnr

---
+++ MONUMENT/Assets/PoppyBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MONUMENT
+{
+    public class PoppyBehaviour : MonoBehaviour
+    {
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Cube")) 
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
