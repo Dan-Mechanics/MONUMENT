d9c1951c364467fb68effc54987fb1e95b12a714
Dan-Mechanics <github.tim@desmidt.org>
2025-03-24 17:00:11 +0100

    good

---
+++ MONUMENT/Assets/EasyTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MONUMENT
+{
+    [RequireComponent(typeof(Collider))]
+    public class EasyTrigger : MonoBehaviour
+    {
+        [SerializeField] private string targetTag = default;
+        [SerializeField] private UnityEvent onTrigger = default;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(targetTag))
+                return;
+
+            onTrigger?.Invoke();
+        }
+    }
+}
